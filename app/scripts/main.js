51baf59ee048e78246ebe87df49357d34f1062ed
Paul Mohr <github@sopamo.de>
2014-10-01 19:20:43 +0200

    Added promo page Fixed save

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -98,7 +98,6 @@ $("#save-form").submit(function(e) {
     var password = $("#password").val();
     var level_data = levelMaker.getLevelString();
     var level_name = $("#level-name").val();
-
     if(user!="" && password!="" && level_name!="") {
 
         levelMaker.saveLevel();
@@ -107,7 +106,7 @@ $("#save-form").submit(function(e) {
         $("#table-body").append(levelMaker.getHistoryEntryHtml(levelMaker.getLevelJson()));
 
 
-        $.post("save.php", {
+        $.post("http://triangula.papaya-fiction.com/save.php", {
             user: user,
             password: password,
             level_name: level_name,
